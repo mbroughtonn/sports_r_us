8cb39876b64efbeac3f5683defceb56a36c1272f
Matt Broughton <mbroughtonn@gmail.com>
2024-12-11 18:32:05 -0600

    Updated Products controller to properly call Products. Updated Brands index view to have links.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.includes(:brand, :category).order("title DESC")
+    @products = Product.includes(:brand, :category).all
   end
 
   def show
